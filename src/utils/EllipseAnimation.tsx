445ab085ba594d647961192eeafb4b2371fa7289
Ari Lotter <arilotter@gmail.com>
2021-11-28 23:55:30 -0500

    ellipse animation should be constant width

--- src/utils/EllipseAnimation.tsx
+++ src/utils/EllipseAnimation.tsx
@@ -1,12 +1,23 @@
 import { useEffect, useState } from "react";
 
+const maxDots = 3;
+
 export function EllipseAnimation({ delay }: { delay?: number }) {
   const [dots, setDots] = useState(0);
   useEffect(() => {
     const interval = setInterval(() => {
-      setDots((dots + 1) % 3);
+      setDots((dots + 1) % maxDots);
     }, delay || 200);
     return () => clearInterval(interval);
   });
-  return <span>{".".repeat(dots + 1)}</span>;
+  return (
+    <span
+      style={{
+        whiteSpace: "pre",
+      }}
+    >
+      {".".repeat(dots + 1)}
+      {" ".repeat(maxDots - (dots + 1))}
+    </span>
+  );
 }
