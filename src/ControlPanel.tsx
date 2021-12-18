75700bb76410b5d2ee9e31c3163aac8e9540d952
Ari Lotter <arilotter@gmail.com>
2021-12-17 19:04:47 -0500

    add git commit info

--- src/ControlPanel.tsx
+++ src/ControlPanel.tsx
@@ -2,8 +2,12 @@ import { ButtonForm, Window } from "packard-belle";
 import { useImmer } from "use-immer";
 import { config, setSetting } from "./settings";
 import { Writeable } from "./utils/utils";
+import GitInfo from "react-git-info/macro";
 
 import controlPanelIcon from "./icons/controlPanel.png";
+
+const gitInfo = GitInfo();
+
 interface ControlPanelProps {
   onClose: () => void;
 }
@@ -22,6 +26,8 @@ export function ControlPanel({ onClose }: ControlPanelProps) {
   return (
     <div className="modal">
       <Window title="Control Panel" icon={controlPanelIcon}>
+        <div style={{ padding: "4px" }}>Version: {gitInfo.commit.hash}</div>
+        <div style={{ padding: "4px" }}>Build date: {gitInfo.commit.date}</div>
         {realKeys.map((configKey) => (
           <div key={configKey} style={{ padding: "4px" }}>
             <label htmlFor={configKey}>{sentanceCaseify(configKey)}</label>
